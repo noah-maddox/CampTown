d4a84559e18939a9257deaf414e0c5c8417db0c2
Noah Maddox <Noah.Maddox@ibm.com>
2019-12-22 10:37:41 -0500

    update app.js for prod use

--- app.js
+++ app.js
@@ -12,32 +12,8 @@ app.get("/campgrounds", function(req, res){
     res.render("campgrounds");
 })
 
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("Yelpcamp server has started!");
+
+// this is the port needed for testing not production
+// app.listen(8080, function(){
+//     console.log("Yelpcamp server has started on port 8080!");
 // });
-
-app.listen(8080, function(){
-    console.log("Yelpcamp server has started on port 8080!");
-});
-
-// var http = require('http');
-// var fs = require('fs');
-// var url = require('url');
-
-// http.createServer( function (request, response) {
-//   var pathname = url.parse(request.url).pathname;
-//   console.log("Trying to find '" + pathname.substr(1) + "'...");
-
-//   fs.readFile(pathname.substr(1), function (err, data) {
-//     if (err) {
-//       response.writeHead(404, {'Content-Type': 'text/html'});
-//       response.write("ERROR: Cannot find '" + pathname.substr(1) + "'.");
-//       console.log("ERROR: Cannot find '" + pathname.substr(1) + "'.");
-//     } else {
-//       console.log("Found '" + pathname.substr(1) + "'.");
-//       response.writeHead(200, {'Content-Type': 'text/html'});
-//       response.write(data.toString());
-//     }
-//     response.end();
-//   });
-// }).listen(8080, 'localhost'); // Or 8081 or 8082 instead of 8080. Or '127.0.0.1' instead of 'localhost'.
